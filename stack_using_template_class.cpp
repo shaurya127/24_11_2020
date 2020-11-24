af8c51fa91eccd946dfcfcacf2572faf8812362f
shaurya127 <63494206+shaurya127@users.noreply.github.com>
2020-11-24 14:35:50 +0530

    Add files via upload
    Stack with Template class

---
+++ stack_using_template_class.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class stack_using_vector
+{
+private:
+    vector<T> v;
+public:
+    void push(T data){
+        v.push_back(data);
+    }
+    bool empty(){
+        return v.size()==0;
+    }
+    void pop(){
+        if(!empty()){
+            v.pop_back();
+        }
+    }
+
+    T top(){
+        return v[v.size()-1];
+    }
+    
+};
+
+
+int main()
+{
+    
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+
+    // stack_using_vector<char> s;
+    stack_using_vector<int>s;
+
+    for (int i = 1; i <=5; ++i)
+    {
+        s.push(i*i);
+    }
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+
+
+    // cout<<"\n\n";
+
+    // s.push('a');
+    // s.push('b');
+    // s.push('c');
+    // cout<<s.top()<<endl;
+    // s.pop();
+    // cout<<s.top()<<endl;
+   
+
+      return 0;
+
+}
